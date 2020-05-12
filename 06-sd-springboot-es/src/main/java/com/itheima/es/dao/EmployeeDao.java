6f64ac0fe35b96526cb7b77019fe76735ac4f3dc
zhanghuan <1789241291@qq.com>
2020-05-12 10:20:09 +0800

    springboot-es的项目中添加了几个test方法

--- 06-sd-springboot-es/src/main/java/com/itheima/es/dao/EmployeeDao.java
+++ 06-sd-springboot-es/src/main/java/com/itheima/es/dao/EmployeeDao.java
@@ -1,7 +1,10 @@
 package com.itheima.es.dao;
 
 import com.itheima.es.domain.Employee;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
 
 /**
  * @author zhang
@@ -9,4 +12,23 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
  * @date 2020/4/19 10:06
  */
 public interface EmployeeDao extends ElasticsearchRepository<Employee, String> {
+
+    /**
+     * 根据年龄区间和姓名查询(默认每页10条数据)
+     * @param start 年龄开始条件
+     * @param end 年龄结束条件
+     * @param name 姓名中包含的字段
+     * @return List<Employee>
+     */
+    List<Employee> findByAgeBetweenAndNameContains(int start,int end,String name);
+
+    /**
+     * 根据年龄区间和姓名查询+分页条件
+     * @param start 年龄开始条件
+     * @param end 年龄结束条件
+     * @param name 姓名中包含的字段
+     * @param pageable 分页参数
+     * @return List<Employee>
+     */
+    List<Employee> findByAgeBetweenAndNameContains(int start, int end, String name, Pageable pageable);
 }
--- 06-sd-springboot-es/src/test/java/com/ithaima/es/test/EsSpringbootTest.java
+++ 06-sd-springboot-es/src/test/java/com/ithaima/es/test/EsSpringbootTest.java
@@ -3,11 +3,17 @@ package com.ithaima.es.test;
 import com.itheima.es.EsApplication;
 import com.itheima.es.dao.EmployeeDao;
 import com.itheima.es.domain.Employee;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -38,12 +44,70 @@ public class EsSpringbootTest {
         //向索引库中添加文档
         for (int i = 1; i <= 20; i++) {
             Employee employee = new Employee();
-            employee.setId(i + "");
-            employee.setAge(20 + i);
-            employee.setName("孙悟空" + i);
-            employee.setAddress("花果山" + i);
+            employee.setId(20+i + "");
+            employee.setAge(40 + i);
+            employee.setName("现任中国共产党中央委员会总书记，中共中央军事委员会主席，中华人民共和国主席，中华人民共和国中央军事委员会主席" + i);
+            employee.setAddress("习近平，男，汉族，1953年6月生，陕西富平人，1969年1月参加工作，1974年1月加入中国共产党，清华大学人文社会学院马克思主义理论与思想政治教育专业毕业，在职研究生学历，法学博士学位" + i);
             list.add(employee);
         }
         employeeDao.saveAll(list);
+    }
+
+    @Test
+    public void deleteDocument() {
+        employeeDao.deleteById("14");
+    }
+
+    @Test
+    public void updateDocument() {
+        //将索引库中ID为19的document中的数据更新为以下
+        Employee employee = new Employee();
+        employee.setId("19");
+        employee.setAge(19);
+        employee.setName("孙悟空");
+        employee.setAddress("花果山");
+        employeeDao.save(employee);
+    }
+
+    /**
+     * 简单查询
+     */
+    @Test
+    public void findSimple(){
+        Iterable<Employee> employeeIterable = employeeDao.findAll(Sort.by(Sort.Order.desc("age")));
+        employeeIterable.forEach(System.out::println);
+    }
+
+    /**
+     * 自定义查询(默认每页10条数据)
+     */
+    @Test
+    public void customFind1(){
+        List<Employee> employees = employeeDao.findByAgeBetweenAndNameContains(30, 60, "中国共产党");
+        System.out.println(employees.size());
+        employees.forEach(System.out::println);
+    }
+
+
+    /**
+     * 自定义查询(通过分页参数流控制显示的条数)
+     */
+    @Test
+    public void customFind2(){
+        List<Employee> employees = employeeDao.findByAgeBetweenAndNameContains(30, 60, "中国共产党", PageRequest.of(0,5));
+        System.out.println(employees.size());
+        employees.forEach(System.out::println);
+    }
+
+    /**
+     * 使用es原生查询对象进行查询
+     */
+    @Test
+    public void originalFind(){
+        //1.构造查询条件对象
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(QueryBuilders.queryStringQuery("清华大学人文社会学院马克思主义理论与思想政治教育专业毕业")).withPageable(PageRequest.of(0,3)).build();
+
+        //2.执行查询
+        elasticsearchTemplate.queryForList(searchQuery,Employee.class).forEach(System.out::println);
     }
 }
